933dca58fd3e66fc83cce5c397fcd1a32b09d005
Matías Israelson <57065102+israpps@users.noreply.github.com>
2026-05-12 21:40:34 -0300

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,18 +1,23 @@
 @echo off
 
-for %%a in (%*) do (
-  if "%%a"=="--setvar" (
-    if exist "%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat" (
-      call "%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-    ) else if exist "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" (
-      call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-    ) else (
-      echo Visual Studio 2022 not found.
-    )
+where msbuild
+if errorlevel 1 (
+  if exist "%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat" (
+    call "%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+  ) else if exist "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" (
+    call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+  ) else (
+    echo Visual Studio 2022 not found.
   )
+)
+
+for %%a in (%*) do (
   if "%%a"=="--clean" (
     msbuild "PCSX2_qt.sln" /m /v:m /p:Configuration=release /t:Clean
+  ) else if "%%a"=="--help" (
+    echo --clean: duh
   )
+  
 )
 
 
