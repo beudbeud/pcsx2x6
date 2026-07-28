17e0752b3653b8fdb4ef2801f3f01101be7eaa86
lightningterror <18107717+lightningterror@users.noreply.github.com>
2026-07-28 10:19:01 +0200

    GS/VK: Fix crash when no device is created and vk is the selected renderer.

--- pcsx2/GS/Renderers/Vulkan/GSDeviceVK.cpp
+++ pcsx2/GS/Renderers/Vulkan/GSDeviceVK.cpp
@@ -2239,8 +2239,11 @@ void GSDeviceVK::Destroy()
 
 	m_swap_chain.reset();
 
-	DestroySpinResources();
-	DestroyResources();
+	if (m_device != VK_NULL_HANDLE)
+	{
+		DestroySpinResources();
+		DestroyResources();
+	}
 
 	VKShaderCache::Destroy();
 
